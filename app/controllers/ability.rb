6a2e60ae6e95e38367bc2ece92251c7c434a6203
Kendra McClanahan <kendramcclanahan@Kendras-MacBook-Pro.local>
2013-12-07 13:45:22 -0600

    Add Ability class with user roles

--- app/controllers/ability.rb
+++ app/controllers/ability.rb
@@ -2,16 +2,34 @@ class Ability
 	include CanCan:Ability
 
 	def initialize(user)
-		user ||= User.new # guest user -- not logged in
-		if user.admin?
-			can: manage, :all
-		elsif user.nonprofit?
+		@user = user || User.new # guest user -- not logged in
+		@user.roles.each { |role| send(role) }
 
+		# Using CRUD alias for nonprofit user
+		alias_action :create, :read, :update, :destroy, :to=> :crud
 
-		elsif user.developer?
-
-		else
-			can :read, :all
+		if @user.roles.size == 0
+			# Need to add what the guest user can do here
+			# Probably nothing, maybe see static pages
 		end
+	end
+
+	def admin
+		can :manage, :all
+	end
+
+	# Sets the nonprofit role
+	#
+	# Nonprofits can manage all CRUD actions
+	# Active - sets CRUD actions to only active projects of that user
+
+	def nonprofit
+		can :crud, Project, :active => true, :user_id => user.id
+	end
+
+	def developer
+		can :read, Project
+	end
+
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   ROLES = %w[admin nonprofit developer]
+  attr_accessible :roles
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
