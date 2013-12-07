994fa974dec674f971e07a4c30eaf61905200587
Kendra McClanahan <kendramcclanahan@Kendras-MacBook-Pro.local>
2013-12-07 14:01:09 -0600

    Ability Controller Functions

--- app/controllers/ability.rb
+++ app/controllers/ability.rb
@@ -2,7 +2,7 @@ class Ability
 	include CanCan:Ability
 
 	def initialize(user)
-		@user = user || User.new # guest user -- not logged in
+		user ||= User.new # guest user -- not logged in
 		@user.roles.each { |role| send(role) }
 
 		# Using CRUD alias for nonprofit user
