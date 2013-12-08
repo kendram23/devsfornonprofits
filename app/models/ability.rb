bd5ec1b57d890eb8a78f8cb0f7c5473bff20bd06
Kendra McClanahan <kendramcclanahan@Kendras-MacBook-Pro.local>
2013-12-07 20:48:31 -0600

    Project views added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,36 +3,30 @@ class Ability
 
     def initialize(user)
         user ||= User.new # guest user -- not logged in
+        
+        if user.role? :admin
+            can: manage, :all?
+        end
 
-        can :manage, :all if user.role == "admin"
-        if user.role == "developer"
+        alias_action :create, :read, :update, :destroy, :to=> :crud
+        
+        # Sets the nonprofit role
+        #
+        # Nonprofits can manage all CRUD actions
+        # Active - sets CRUD actions to only active projects of that user
+
+        if user.role? "developer"
             can :read, Project
             can :crud, User, :active => true, :user_id => user.id
         end
 
-
         if user.role == 'nonprofit'
-
-
-
+            can :crud, Project, :active => true, :user_id => user.id
+            can :crud, User, :active => true, :user_id => user.id
         end
 
         # Using CRUD alias for nonprofit user
-        alias_action :create, :read, :update, :destroy, :to=> :crud
-
-        if user.role == 0
-            # Need to add what the guest user can do here
-            # Probably nothing, maybe see static pages
-        end
     end
 
-    # Sets the nonprofit role
-    #
-    # Nonprofits can manage all CRUD actions
-    # Active - sets CRUD actions to only active projects of that user
-
-    def nonprofit
-        can :crud, Project, :active => true, :user_id => user.id
-    end
 end
 
