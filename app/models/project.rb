300664ced21235ceecf0a7094193da28a5cdbc3b
MakerSquare Student <students@themakersquare.com>
2013-12-08 14:00:24 -0600

    Project Controller changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
 	has_many :current_projects
 	
 	def current_project_applicants(id)
-	x = CurrentProject.find_all_by_user_id(id)
+	x = CurrentProject.find(id)
 		x.each do |list|
 			if list.id == @project.id
 			end
